7d3dcabeab706ddf57d03a24fd88fda9e3e376ec
Abigail <farishiisyamhardimann@gmail.com>
2025-08-25 21:47:53 +0700

    docs(index): tambahan penjelasan komentar & formatting line

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php
+// ===== BLOK LOGIKA & KEAMANAN =====
+
+// 1. Menyertakan file koneksi database. 
+//    Ini adalah praktik yang baik untuk memisahkan kredensial database dari logika utama.
 include ('koneksi.php');
+
+// 2. Memulai sesi PHP. 
+//    Dibutuhkan untuk mengakses variabel session seperti $_SESSION['username'].
 session_start();
+
+// 3. Pengecekan Sesi (Gerbang Keamanan).
+//    Memastikan bahwa hanya pengguna yang sudah login (memiliki session 'username') 
+//    yang dapat mengakses halaman ini. Jika tidak, akan dialihkan ke halaman login.
 if (!isset($_SESSION['username'])) {
     header("Location: login.php");
-    exit();
+    exit(); // Menghentikan eksekusi skrip setelah redirect.
 }
+
+// 4. Pengambilan Data dari Database.
+//    Menjalankan query SQL untuk mengambil semua data dari tabel 'teks'.
+//    Hasilnya disimpan di variabel $queryTeks untuk digunakan nanti di bagian HTML.
 $queryTeks = mysqli_query($conn, "SELECT * FROM teks");
 ?>
 
@@ -13,103 +28,68 @@ $queryTeks = mysqli_query($conn, "SELECT * FROM teks");
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
+    <meta name="description" content="Sistem Informasi UNTAT">
+    <meta name="author" content="anomali">
     <title>Web Akademik UNTAT</title>
-
-    <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
 </head>
 
 <body id="page-top">
-
-    <!-- Page Wrapper -->
     <div id="wrapper">
-
-        <!-- Sidebar -->
-        <?php include 'sidebar.php'; ?>
-        <!-- End of Sidebar -->
-
-        <!-- Content Wrapper -->
+        <?php
+        // Praktik terbaik: Memisahkan bagian-bagian yang berulang (seperti sidebar)
+        // ke dalam file terpisah agar mudah dikelola dan digunakan di banyak halaman.
+        include 'sidebar.php';
+        ?>
         <div id="content-wrapper" class="d-flex flex-column">
-
-            <!-- Main Content -->
             <div id="content">
-
-                <!-- Topbar -->
-                <?php include 'topbar.php'; ?>
-                <!-- End of Topbar -->
-
-                <!-- Begin Page Content -->
+                <?php
+                // Sama seperti sidebar, topbar juga dipisahkan menjadi komponen sendiri.
+                include 'topbar.php';
+                ?>
                 <div class="container-fluid">
-
-                    <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-                        
                     </div>
                     <div class="row">
                         <div class="col-lg-12 mb-4">
-                            <!-- Approach -->
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
                                     <h6 class="m-0 font-weight-bold text-primary">Kata Pengantar</h6>
                                 </div>
                                 <div class="card-body">
                                 <?php
-                                    $data=mysqli_fetch_array($queryTeks);
-                                ?>    
-                                <P><?php echo $data['teks_penjelasan']; ?></P>
+                                    // Mengambil satu baris data dari hasil query yang sudah dijalankan di atas.
+                                    $data = mysqli_fetch_array($queryTeks);
+                                ?>
+                                <p><?php echo $data['teks_penjelasan']; ?></p>
                                 </div>
                             </div>
-
                         </div>
                     </div>
 
                 </div>
-            </div>
+                </div>
             <?php include "footer.php"; ?>
         </div>
-    </div>
-    <!-- End of Page Wrapper -->
-
-    <!-- Scroll to Top Button-->
+        </div>
     <a class="scroll-to-top rounded" href="#page-top">
         <i class="fas fa-angle-up"></i>
     </a>
 
-    <!-- Logout Modal-->
     <?php include "modal.php" ?>
 
-    <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
     <script src="vendor/chart.js/Chart.min.js"></script>
-
-    <!-- Page level custom scripts -->
     <script src="js/demo/chart-area-demo.js"></script>
     <script src="js/demo/chart-pie-demo.js"></script>
-
 </body>
-
 </html>
