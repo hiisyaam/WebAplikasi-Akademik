44009545a56ed35fa2565a4a8e5149d4fc59033f
Abigail <farishiisyamhardimann@gmail.com>
2025-08-25 22:10:11 +0700

    docs(sidebar, topbar): tambahan penjelasan komentar & formatting line

--- sidebar.php
+++ sidebar.php
@@ -1,85 +1,69 @@
-
-
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
-                <div class="sidebar-brand-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
-                        <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
-                        <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
-                </svg>
-                </div>
-                <div class="sidebar-brand-text mx-2 mt-1">Web Akademik</div>
-            </a>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <a class="nav-link" href="index.php">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+                <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
+                <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
+            </svg>
+        </div>
+        <div class="sidebar-brand-text mx-2 mt-1">Web Akademik</div>
+    </a>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+    <hr class="sidebar-divider my-0">
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Menu Admin
+    <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider">
+
+    <div class="sidebar-heading">
+        Menu Admin
+    </div>
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+            aria-expanded="true" aria-controls="collapsePages">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Menu Admin</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Menu:</h6>
+                <a class="collapse-item" href="logout.php">Log Out</a>
             </div>
+        </div>
+    </li>
 
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                    aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Menu Admin</span>
-                </a>
-                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Menu:</h6>
-                        <a class="collapse-item" href="logout.php">Log Out</a>
-                    </div>
-                </div>
-            </li>
+    <hr class="sidebar-divider">
 
+    <div class="sidebar-heading">
+        Data Mahasiswa
+    </div>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+    <li class="nav-item">
+        <a class="nav-link" href="list_data.php">
+            <i class="fas fa-fw fa-table"></i>
+            <span>List Mahasiswa</span>
+        </a>
+    </li>
+    
+    <li class="nav-item">
+        <a class="nav-link" href="tambahMahasiswa.php">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Tambah Mahasiswa</span>
+        </a>
+    </li>
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Data Mahasiswa
-            </div>
+    <hr class="sidebar-divider d-none d-md-block">
 
-            <!-- Nav Item - Pages Collapse Menu -->
-            
-
-            <!-- Nav Item - Charts -->
-
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="list_data.php">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>List Mahasiswa</span></a>
-            </li>
-            
-            <li class="nav-item">
-                <a class="nav-link" href="tambahMahasiswa.php">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tambah Mahasiswa</span></a>
-            </li>
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
-
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-
-            <!-- Sidebar Message -->
-
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
 </ul>
--- topbar.php
+++ topbar.php
@@ -1,54 +1,59 @@
 <?php
+/**
+ * File: topbar.php
+ * Deskripsi: Komponen ini menampilkan bar navigasi atas, termasuk
+ * nama pengguna yang sedang login dan menu dropdown profil.
+ */
+
+// !!! KESALAHAN LOGIKA PENTING !!!
+// Kode di bawah ini salah karena mengambil SEMUA pengguna dan hanya menampilkan
+// pengguna pertama dari tabel. Seharusnya, nama pengguna diambil dari
+// variabel SESSION yang sudah dibuat saat login.
+
+/*
+    // KODE YANG SALAH (JANGAN DIGUNAKAN):
     require "koneksi.php";
     $query = mysqli_query($conn, "SELECT * FROM users");
     $data = mysqli_fetch_array($query);
+*/
+
+// CARA YANG BENAR:
+// Ambil nama pengguna dari session yang aktif.
+// Pastikan session_start() sudah dipanggil di file utama (seperti index.php).
+$loggedInUsername = $_SESSION['username'] ?? 'Guest'; // Gunakan 'Guest' jika session tidak ada
+
 ?>
 
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-                    <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
+    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+        <i class="fa fa-bars"></i>
+    </button>
+    
+    <ul class="navbar-nav ml-auto">
 
-                    <!-- Topbar Search -->
+        <div class="topbar-divider d-none d-sm-block"></div>
 
-                    <!-- Topbar Navbar -->
-                    <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown no-arrow">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Halo, <?php echo htmlspecialchars($loggedInUsername); ?></span>
+                
+                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
+            </a>
+            
+            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                <a class="dropdown-item" href="#" onclick="alert('Belum Dibangun')">
+                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Profile
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Logout
+                </a>
+            </div>
+        </li>
 
-                        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                            
-
-                        <!-- Nav Item - Alerts -->
-                        
-
-                        <!-- Nav Item - Messages -->
-                        
-
-                        <div class="topbar-divider d-none d-sm-block"></div>
-
-                        <!-- Nav Item - User Information -->
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Halo, <?php echo  $data['username']; ?></span>
-                                <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
-                            </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#" onclick="alert('Belum Dibangun')">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
-                                </a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="logout.php" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
-                            </div>
-                        </li>
-
-                    </ul>
-
-                </nav>
+    </ul>
+</nav>
